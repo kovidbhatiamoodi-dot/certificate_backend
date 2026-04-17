364364d91f9288213beca5a9c9958f3bb8a774f1
Kovid Bhatia <kovidbhatiamoodi@gmail.com>
2026-04-18 04:22:46 +0530

    port update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 const initDB = require("./config/initDb");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5051;
 
 // Initialize DB then start server
 initDB().then(() => {
